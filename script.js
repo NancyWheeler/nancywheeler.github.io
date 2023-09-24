ae66b95bb8cf0e4f2e553c758dff976ffc9e1255
Nancy Wheeler <107950648+NancyWheeler@users.noreply.github.com>
2023-09-23 22:29:52 -0400

    New webpage layout and content

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-window.addEventListener("load",function() { changeBackground() });
-
-function changeBackground() {
-    document.body.style.background = randomColor();
-}
+window.addEventListener("load", () => { 
+    document.body.style.background = randomColor(); 
+});
 
 function rand(min, max) {
     let randomNum = Math.random() * (max - min) + min;
     return Math.round(randomNum);
 }
 
-var generateColor = function () {
-    // hex numbers 
-    var hex = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    var color = '#';
-    // we will generate hex color with 6 digit length
-    for (var i = 0; i < 6; i++) {
-        let index = rand(0,15);
-        // Append hex value at the index
-        color += hex[index];
-    }
-    return color;
-};
+// // var generateColor = function () {
+// //     // hex numbers 
+// //     var hex = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// //     var color = '#';
+// //     // we will generate hex color with 6 digit length
+// //     for (var i = 0; i < 6; i++) {
+// //         let index = rand(0,15);
+// //         // Append hex value at the index
+// //         color += hex[index];
+// //     }
+// //     return color;
+// // };
 
 var randomColor = function () {
     var colors = ['#4C48B0', '#7285D8', '#FFC1BA', '#FFE8D6', '#CD5C84', 
